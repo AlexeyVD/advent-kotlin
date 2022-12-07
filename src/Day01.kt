d65d237987c4786e4588c5c8e43f0da4f95de493
aleksey.dementyev <ad@primexbt.ch>
2022-12-07 20:57:33 +0300

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return getMaxSum(input, 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getMaxSum(input, 3)
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun getMaxSum(input: List<String>, entries: Int): Int {
+    val result = ArrayList<Int>()
+    var current = 0
+    input.forEach {
+        if (it.isBlank()) {
+            result.add(current)
+            current = 0
+        } else {
+            current += it.toInt()
+        }
+    }
+
+    if (current != 0)
+        result.add(current)
+
+    result.sort()
+    return result.takeLast(entries).sum()
 }
